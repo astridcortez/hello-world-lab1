e7dc61c90b978fcc15ca7f4f846a24007751dfb0
Esther Cortez <70614557+astridcortez@users.noreply.github.com>
2020-09-02 05:23:39 +0300

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -1,7 +1,10 @@
+
+//simple java program that prints out Hello World! when it is run
 public class lab1{
 
+  //the main program
   public static void main (String args[]){
-    System.out.println("Hello World");
+    System.out.println("Hello World!"); //display the string Hello World!
   
   }
 }
